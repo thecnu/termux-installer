2876118f318feab3049b8714f2c8f2e9e76b8251
caner <107654153+thecnu@users.noreply.github.com>
2024-05-01 16:02:11 +0300

    tx.sh

--- tx.sh
+++ tx.sh
@@ -7,12 +7,7 @@ pkg install git curl
 pkg upgrade -y
 pkg install git -y
 pkg install python -y
-pip install cython
-pkg install libxml2 libxslt -y
-pkg install -y python ndk-sysroot clang make libjpeg-turbo -y
-pkg install clang -y
-pip install lxml
-pip install --pre uiautomator2
+
 
 apt-get update
 pkg install wget
