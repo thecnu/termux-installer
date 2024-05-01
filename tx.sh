1a2a4b4df1703ada45dc56c7dababeda604bd56e
caner <107654153+thecnu@users.noreply.github.com>
2024-05-01 16:21:01 +0300

    tx.sh

--- tx.sh
+++ tx.sh
@@ -19,7 +19,7 @@ apt update && apt full-upgrade && pip install requests
 
 # dosyayı indir
 mkdir storage/downloads/testapp
-cd storage/downloads/testapp
+
 curl -O <https://objects.githubusercontent.com/github-production-release-asset-2e65be/53297769/1de4c4fd-878a-4dc3-a13c-ea0996f61b79?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240501%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240501T124510Z&X-Amz-Expires=300&X-Amz-Signature=416be384d9aa0a800c5e53a989dfc4033681a505b417f68457aaaf6aa06ab82a&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=53297769&response-content-disposition=attachment%3B%20filename%3Dappium-uiautomator2-server-debug-androidTest.apk&response-content-type=application%2Fvnd.android.package-archive>
 curl -O <https://objects.githubusercontent.com/github-production-release-asset-2e65be/53297769/caef2ae0-7978-463b-b0e6-b24617a4a03b?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20240501%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240501T124506Z&X-Amz-Expires=300&X-Amz-Signature=987786da010d141e413b19d10ec87c898cb25c08d95c8187a1f9eea32ce07a7d&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=53297769&response-content-disposition=attachment%3B%20filename%3Dappium-uiautomator2-server-v7.0.5.apk&response-content-type=application%2Fvnd.android.package-archive>
 
